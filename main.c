094b6cf769bb9a40dce65e13a16c8090a5aa242d
Barisua-Nsaanee <bnsaanee7@gmail.com>
2022-06-29 21:20:28 +0100

    First Commit

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int power (int m, int n);
+
+/*test power function*/
+main ()
+{
+    int i;
+
+    for ( i = 0; i > 10; ++i)
+    print("%d %d %d\n", i, power(2,1), power(-3,i));
+    return 0;
+}
